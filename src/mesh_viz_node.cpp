933259af594f2d7fde92fd7aa9c06b1b32b42b38
EnricoMingo <enricomingo@gmail.com>
2019-10-28 19:25:49 +0100

    Added collision object msg. Needs some refactor for names used for markers and stuffs

--- src/mesh_viz_node.cpp
+++ src/mesh_viz_node.cpp
@@ -1,10 +1,16 @@
 #include <ros/ros.h>
 #include <tf/tf.h>
 #include <interactive_markers/interactive_marker_server.h>
+#include <moveit_msgs/CollisionObject.h>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
+#include <geometric_shapes/mesh_operations.h>
+#include <geometric_shapes/shape_messages.h>
+#include <geometric_shapes/shape_operations.h>
 
 using namespace visualization_msgs;
+
+ros::Publisher collision_object_publisher;
 
 void processFeedback(
     const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
@@ -42,35 +48,35 @@ InteractiveMarkerControl& makeBoxControl( InteractiveMarker &msg, const std::str
   return msg.controls.back();
 }
 
-void make6DofMarker( bool fixed, unsigned int interaction_mode, const tf::Vector3& position, bool show_6dof,
+InteractiveMarker::Ptr make6DofMarker( bool fixed, unsigned int interaction_mode, const tf::Vector3& position, bool show_6dof,
                      const std::string& mesh_path, const double scale, const std::string& frame_id,
                      interactive_markers::InteractiveMarkerServer& server)
 {
-  InteractiveMarker int_marker;
-  int_marker.header.frame_id = frame_id;
-  tf::pointTFToMsg(position, int_marker.pose.position);
-  int_marker.scale = 1;
+  InteractiveMarker::Ptr int_marker = boost::make_shared<InteractiveMarker>();
+  int_marker->header.frame_id = frame_id;
+  tf::pointTFToMsg(position, int_marker->pose.position);
+  int_marker->scale = 1;
 
   std::vector< std::string > vec_string;
-  boost::split(vec_string, mesh_path, boost::is_any_of("/"), boost::token_compress_on);
+  boost::split(vec_string, mesh_path, boost::is_any_of("/,."), boost::token_compress_on);
 
   if(vec_string.empty())
-      int_marker.name = mesh_path;
+      int_marker->name = mesh_path;
   else
-      int_marker.name = vec_string.back();
+      int_marker->name = vec_string.back();
 
-  int_marker.description = "Mesh 6-DOF Control";
+  int_marker->description = "Mesh 6-DOF Control";
 
   // insert a box
-  makeBoxControl(int_marker, mesh_path, scale);
-  int_marker.controls[0].interaction_mode = interaction_mode;
+  makeBoxControl(*int_marker, mesh_path, scale);
+  int_marker->controls[0].interaction_mode = interaction_mode;
 
   InteractiveMarkerControl control;
 
   if ( fixed )
   {
-    int_marker.name += "_fixed";
-    int_marker.description += "\n(fixed orientation)";
+    int_marker->name += "_fixed";
+    int_marker->description += "\n(fixed orientation)";
     control.orientation_mode = InteractiveMarkerControl::FIXED;
   }
 
@@ -80,8 +86,8 @@ void make6DofMarker( bool fixed, unsigned int interaction_mode, const tf::Vector
       if( interaction_mode == visualization_msgs::InteractiveMarkerControl::MOVE_3D )         mode_text = "MOVE_3D";
       if( interaction_mode == visualization_msgs::InteractiveMarkerControl::ROTATE_3D )       mode_text = "ROTATE_3D";
       if( interaction_mode == visualization_msgs::InteractiveMarkerControl::MOVE_ROTATE_3D )  mode_text = "MOVE_ROTATE_3D";
-      int_marker.name += "_" + mode_text;
-      int_marker.description = std::string("3D Control") + (show_6dof ? " + 6-DOF controls" : "") + "\n" + mode_text;
+      int_marker->name += "_" + mode_text;
+      int_marker->description = std::string("3D Control") + (show_6dof ? " + 6-DOF controls" : "") + "\n" + mode_text;
   }
 
   if(show_6dof)
@@ -92,10 +98,10 @@ void make6DofMarker( bool fixed, unsigned int interaction_mode, const tf::Vector
     control.orientation.z = 0;
     control.name = "rotate_x";
     control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-    int_marker.controls.push_back(control);
+    int_marker->controls.push_back(control);
     control.name = "move_x";
     control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-    int_marker.controls.push_back(control);
+    int_marker->controls.push_back(control);
 
     control.orientation.w = 1;
     control.orientation.x = 0;
@@ -103,10 +109,10 @@ void make6DofMarker( bool fixed, unsigned int interaction_mode, const tf::Vector
     control.orientation.z = 0;
     control.name = "rotate_z";
     control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-    int_marker.controls.push_back(control);
+    int_marker->controls.push_back(control);
     control.name = "move_z";
     control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-    int_marker.controls.push_back(control);
+    int_marker->controls.push_back(control);
 
     control.orientation.w = 1;
     control.orientation.x = 0;
@@ -114,16 +120,17 @@ void make6DofMarker( bool fixed, unsigned int interaction_mode, const tf::Vector
     control.orientation.z = 1;
     control.name = "rotate_y";
     control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
-    int_marker.controls.push_back(control);
+    int_marker->controls.push_back(control);
     control.name = "move_y";
     control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-    int_marker.controls.push_back(control);
+    int_marker->controls.push_back(control);
   }
 
-  server.insert(int_marker);
-  server.setCallback(int_marker.name, &processFeedback);
+  server.insert(*int_marker);
+  server.setCallback(int_marker->name, &processFeedback);
 //  if (interaction_mode != visualization_msgs::InteractiveMarkerControl::NONE)
 //    menu_handler.apply( server, int_marker.name );
+  return int_marker;
 }
 
 
@@ -159,11 +166,31 @@ int main(int argc, char** argv)
   interactive_markers::InteractiveMarkerServer server("simple_marker");
 
   tf::Vector3 position(0,0,0);
-  make6DofMarker(false, visualization_msgs::InteractiveMarkerControl::MOVE_ROTATE_3D,
+  InteractiveMarker::Ptr int_marker = make6DofMarker(false, visualization_msgs::InteractiveMarkerControl::MOVE_ROTATE_3D,
                  position, true, mesh_path, scale, frame_id, server);
 
   // 'commit' changes and send to all clients
   server.applyChanges();
+
+  // ADD to moveit CollisionObject
+  moveit_msgs::CollisionObject co;
+  co.header.frame_id = int_marker->header.frame_id;
+  co.id = int_marker->name;
+  co.header.stamp = ros::Time::now();
+
+  shapes::ShapeMsg mesh_msg;
+  shapes::constructMsgFromShape(shapes::createMeshFromResource(mesh_path, scale*Eigen::Vector3d::Ones()), mesh_msg);
+  co.meshes.push_back(boost::get<shape_msgs::Mesh>(mesh_msg));
+  geometry_msgs::Pose pose;
+  pose.position.x = 0.0; pose.position.y = 0.0; pose.position.z = 0.0;
+  pose.orientation.w = 1.0; pose.orientation.x = 0.0; pose.orientation.y = 0.0; pose.orientation.z = 0.0;
+  co.mesh_poses.push_back(pose);
+
+  co.operation = co.ADD;
+  collision_object_publisher = n.advertise<moveit_msgs::CollisionObject>(int_marker->name, 0, true);
+
+  collision_object_publisher.publish(co);
+
 
   // start the ROS main loop
   ros::spin();
