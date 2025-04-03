02b4fc478b7c7a12961f708debb9e21967b0ba6f
EnricoMingo <enricomingo@gmail.com>
2025-04-03 16:40:47 +0200

    Added planning scene publisher to be used with cartesio_collision_support

---
+++ src/ps_pub.cpp
@@ -0,0 +1,88 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2012, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Enrico Mingo Hoffman */
+
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+
+// MoveIt!
+#include <moveit_msgs/PlanningScene.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/GetStateValidity.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/ApplyPlanningScene.h>
+
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/robot_state/conversions.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+
+ros::ServiceClient srv;
+
+
+
+void collision_object_cb(const moveit_msgs::CollisionObjectConstPtr msg)
+{
+    moveit_msgs::CollisionObject collision_object = *msg;
+
+    collision_object.header.frame_id = "world";
+
+    srv.waitForExistence();
+
+    moveit_msgs::ApplyPlanningScene msg2;
+    msg2.request.scene.is_diff = true;
+    msg2.request.scene.world.collision_objects = {collision_object};
+
+
+    srv.call(msg2);
+}
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "ps_pub");
+
+    ros::NodeHandle node_handle, nhpr("~");
+
+
+    ros::Subscriber sub = node_handle.subscribe("cartesian/collision_objects", 1, collision_object_cb);
+
+
+    srv = node_handle.serviceClient<moveit_msgs::ApplyPlanningScene>("cartesian/collision_avoidance/apply_planning_scene");
+
+
+    ros::spin();
+
+}
